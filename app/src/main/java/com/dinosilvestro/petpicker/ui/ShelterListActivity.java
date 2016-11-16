bc6d564992db5c41ffd893ddfaca130183d05ca2
dinosilvestro <dino@dinosilvestro.com>
2016-11-16 13:56:58 -0500

    feat: Convert ShelterListActivity to a fragment

--- app/src/main/java/com/dinosilvestro/petpicker/ui/ShelterListActivity.java
+++ app/src/main/java/com/dinosilvestro/petpicker/ui/ShelterListActivity.java
@@ -2,57 +2,21 @@ package com.dinosilvestro.petpicker.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.dinosilvestro.petpicker.R;
-import com.dinosilvestro.petpicker.adapters.ShelterAdapter;
-import com.dinosilvestro.petpicker.fetch.FetchData;
-import com.dinosilvestro.petpicker.fetch.Keys;
-import com.dinosilvestro.petpicker.parcels.ShelterParcel;
-
-import java.util.Arrays;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ShelterListActivity extends AppCompatActivity {
-
-    @BindView(R.id.shelterRecyclerView)
-    RecyclerView mRecyclerView;
-    @BindView(R.id.shelterEmptyCardView)
-    CardView mShelterEmptyCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shelters);
-        ButterKnife.bind(this);
-
-        if (!FetchData.mShelterFlag) {
-            mShelterEmptyCardView.setVisibility(View.VISIBLE);
-        } else {
-            Intent intent = getIntent();
-            Parcelable[] parcelables = intent.getParcelableArrayExtra(Keys.GET_SHELTERS);
-            ShelterParcel[] shelters = Arrays.copyOf(parcelables, parcelables.length, ShelterParcel[].class);
-
-            ShelterAdapter adapter = new ShelterAdapter(this, shelters);
-            mRecyclerView.setAdapter(adapter);
-
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-            mRecyclerView.setLayoutManager(layoutManager);
-
-            mRecyclerView.setHasFixedSize(true);
-        }
-
-        // Set title of actionbar to the zip code passed in
-        setTitle(getString(R.string.pet_shelters_near_text) + MainActivity.mDefaultZipCode);
+        setContentView(R.layout.activity_holder);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ShelterListFragment())
+                .commit();
     }
 
     @Override
---
+++ app/src/main/java/com/dinosilvestro/petpicker/ui/ShelterListFragment.java
@@ -0,0 +1,59 @@
+package com.dinosilvestro.petpicker.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dinosilvestro.petpicker.R;
+import com.dinosilvestro.petpicker.adapters.ShelterAdapter;
+import com.dinosilvestro.petpicker.fetch.FetchData;
+import com.dinosilvestro.petpicker.fetch.Keys;
+import com.dinosilvestro.petpicker.parcels.ShelterParcel;
+
+import java.util.Arrays;
+
+public class ShelterListFragment extends Fragment {
+
+    public ShelterListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_shelters, container, false);
+
+        RecyclerView mRecyclerView = (RecyclerView) rootView.findViewById(R.id.shelterRecyclerView);
+        CardView mShelterEmptyCardView = (CardView) rootView.findViewById(R.id.shelterEmptyCardView);
+
+        if (!FetchData.mShelterFlag) {
+            mShelterEmptyCardView.setVisibility(View.VISIBLE);
+        } else {
+            Intent intent = getActivity().getIntent();
+            Parcelable[] parcelables = intent.getParcelableArrayExtra(Keys.GET_SHELTERS);
+            ShelterParcel[] shelters = Arrays.copyOf(parcelables, parcelables.length, ShelterParcel[].class);
+
+            ShelterAdapter adapter = new ShelterAdapter(getActivity(), shelters);
+            mRecyclerView.setAdapter(adapter);
+
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+            mRecyclerView.setLayoutManager(layoutManager);
+
+            mRecyclerView.setHasFixedSize(true);
+        }
+
+        // Set title of actionbar to the zip code passed in
+        getActivity().setTitle(getString(R.string.pet_shelters_near_text) + MainActivity.mDefaultZipCode);
+
+        return rootView;
+    }
+}
