c54edcc6e303eaa27cddc1820c1a1e8215cc2336
dinosilvestro <dino@dinosilvestro.com>
2016-11-16 16:37:44 -0500

    feat: Convert PetGridActivity to a fragment

--- app/src/main/java/com/dinosilvestro/petpicker/ui/PetGridActivity.java
+++ app/src/main/java/com/dinosilvestro/petpicker/ui/PetGridActivity.java
@@ -2,61 +2,21 @@ package com.dinosilvestro.petpicker.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.dinosilvestro.petpicker.R;
-import com.dinosilvestro.petpicker.adapters.PetAdapter;
-import com.dinosilvestro.petpicker.fetch.FetchData;
-import com.dinosilvestro.petpicker.fetch.Keys;
-import com.dinosilvestro.petpicker.parcels.PetParcel;
-
-import java.util.Arrays;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class PetGridActivity extends AppCompatActivity {
-
-    @BindView(R.id.petRecyclerView)
-    RecyclerView mPetRecyclerView;
-    @BindView(R.id.emptyCardView)
-    CardView mEmptyCardView;
-    @BindView(R.id.petGridEmptyTextView)
-    TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pet_grid);
-        ButterKnife.bind(this);
-
-        if (!FetchData.mPetFlag) {
-            mEmptyCardView.setVisibility(View.VISIBLE);
-            mEmptyTextView.setText(R.string.empty_adoption_grid_text);
-        } else {
-            Intent intent = getIntent();
-            Parcelable[] parcelables = intent.getParcelableArrayExtra(Keys.GET_PETS);
-            PetParcel[] petParcels = Arrays.copyOf(parcelables, parcelables.length, PetParcel[].class);
-
-            PetAdapter adapter = new PetAdapter(this, petParcels);
-            mPetRecyclerView.setAdapter(adapter);
-
-            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
-            mPetRecyclerView.setLayoutManager(layoutManager);
-
-            mPetRecyclerView.setHasFixedSize(true);
-
-            String shelterName = intent.getStringExtra(Keys.NEW_SHELTER_NAME);
-            setTitle(shelterName);
-        }
+        setContentView(R.layout.activity_holder);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PetGridFragment())
+                .commit();
     }
 
     @Override
---
+++ app/src/main/java/com/dinosilvestro/petpicker/ui/PetGridFragment.java
@@ -0,0 +1,67 @@
+package com.dinosilvestro.petpicker.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dinosilvestro.petpicker.R;
+import com.dinosilvestro.petpicker.adapters.PetAdapter;
+import com.dinosilvestro.petpicker.fetch.FetchData;
+import com.dinosilvestro.petpicker.fetch.Keys;
+import com.dinosilvestro.petpicker.parcels.PetParcel;
+
+import java.util.Arrays;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PetGridFragment extends Fragment {
+
+    public PetGridFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_pet_grid, container, false);
+
+        RecyclerView mPetRecyclerView = (RecyclerView) rootView.findViewById(R.id.petRecyclerView);
+        CardView mEmptyCardView = (CardView) rootView.findViewById(R.id.emptyCardView);
+        TextView mEmptyTextView = (TextView) rootView.findViewById(R.id.petGridEmptyTextView);
+
+        if (!FetchData.mPetFlag) {
+            mEmptyCardView.setVisibility(View.VISIBLE);
+            mEmptyTextView.setText(R.string.empty_adoption_grid_text);
+        } else {
+            Intent intent = getActivity().getIntent();
+            Parcelable[] parcelables = intent.getParcelableArrayExtra(Keys.GET_PETS);
+            PetParcel[] petParcels = Arrays.copyOf(parcelables, parcelables.length, PetParcel[].class);
+
+            PetAdapter adapter = new PetAdapter(getActivity(), petParcels);
+            mPetRecyclerView.setAdapter(adapter);
+
+            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+            mPetRecyclerView.setLayoutManager(layoutManager);
+
+            mPetRecyclerView.setHasFixedSize(true);
+
+            String shelterName = intent.getStringExtra(Keys.NEW_SHELTER_NAME);
+
+            // Set title of actionbar to the name of the shelter
+            getActivity().setTitle(shelterName);
+        }
+
+        return rootView;
+    }
+
+}
